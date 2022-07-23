b9eeb268539d271c628646b1f29c090ceafb17ea
YeonwooSung <neos960518@gmail.com>
2022-07-23 16:17:38 +0900

    Update code architecture

--- app/main.py
+++ app/main.py
@@ -4,12 +4,23 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
+from .api import users
+
 
 app = FastAPI()
-app.mount("/static", StaticFiles(directory="static"), name="static")
+app.mount("/public", StaticFiles(directory="public"), name="public")
 templates = Jinja2Templates(directory="views")
+
+#-------------------------------------------------------------
+# Include routers
+app.include_router(users.router)
+#-------------------------------------------------------------
 
 
 @app.get('/')
 async def index(request: Request):
     return templates.TemplateResponse("item.html")
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
